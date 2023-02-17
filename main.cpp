dad549174ce969778b3d59d19686cb32a07ff3b8
Adelmo Menezes <adelmo.msp@gmail.com>
2023-02-17 14:01:20 -0300

    1 - Introdução a orientação a objetos

--- main.cpp
+++ main.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
+#include <string>
+#include <cstdlib>
 
 using namespace std;
 
+class Carro {
+    public:
+        string marca;
+        int ano;
+    private:
+        string cor;
+};
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    Carro c1;
+    c1.marca = "Fiat";
+    c1.ano = 2017;
+
+    Carro c2;
+    c2.marca = "Ferrari";
+    c2.ano = 2015;
+
+    cout << c1.marca << endl;
+    cout << c2.marca << endl;
+
     return 0;
 }
