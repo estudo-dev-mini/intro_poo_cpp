b2eab2f394761977aebdb772be1dca83a6b64e6e
Adelmo Menezes <adelmo.msp@gmail.com>
2023-02-17 14:37:55 -0300

    3 - Arquivos de Cabeçalho

---
+++ include/Carro.h
@@ -0,0 +1,20 @@
+#ifndef CARRO_H
+#define CARRO_H
+
+#include <string>
+using namespace std;
+
+class Carro
+{
+    public:
+        void setMarca(string marca);
+        string getMarca();
+        void setAno(int ano);
+        int getAno();
+
+    private:
+        string marca;
+        int ano;
+};
+
+#endif // CARRO_H
--- main.cpp
+++ main.cpp
@@ -4,30 +4,7 @@
 
 using namespace std;
 
-class Carro {
-    private:
-        string marca;
-        int ano;
-
-    public:
-        void setMarca(string x) {
-            marca = x;
-        }
-        string getMarca() {
-            return marca;
-        }
-
-        void setAno(int y) {
-            if (y > 1990) {
-                ano = y;
-            } else {
-                ano = 1990;
-            }
-        }
-        int getAno() {
-            return ano;
-        }
-};
+#include "Carro.h"
 
 int main()
 {
